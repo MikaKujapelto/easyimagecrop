37b9eb714e7621d95beb235c12b1ee3bea2a3e66
MikaKujapelto <mika.kujapelto@gmail.com>
2017-02-03 13:38:34 +0100

    Keep aspect ratio as default.

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var easyimg = require('easyimage');
 
 easyimg.crop({
      src:'*.jpg', dst:'./output/image.jpg',
-     cropwidth:900, cropheight:300,
-     ignoreAspectRatio:true
+     cropwidth:900, cropheight:300
   }).then(
   function(image) {
      console.log('Resized and cropped: ' + image.width + ' x ' + image.height);
