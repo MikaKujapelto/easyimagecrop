1eb41ba2ac759785547ce70bbe6ca367839a6d70
MikaKujapelto <mika.kujapelto@gmail.com>
2017-02-03 13:33:22 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Image cropper
+Based on the <a href="https://www.npmjs.com/package/easyimage" target="_blank">easyimage npm package</a>. Need to have imagemagick and node installed on your system.
+
+## Usage
+Clone repo and run npm install. 
+Run the app with `node app.js`    
---
+++ app.js
@@ -0,0 +1,14 @@
+var easyimg = require('easyimage');
+
+easyimg.crop({
+     src:'*.jpg', dst:'./output/image.jpg',
+     cropwidth:900, cropheight:300,
+     ignoreAspectRatio:true
+  }).then(
+  function(image) {
+     console.log('Resized and cropped: ' + image.width + ' x ' + image.height);
+  },
+  function (err) {
+    console.log(err);
+  }
+);
